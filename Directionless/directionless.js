2f56f33f5886778f8653fb055fdee8ed211c5698
Christina Nguyen <phuonghien716@gmail.com>
2016-09-22 03:23:44 -0400

    Added bread object

--- Directionless/directionless.js
+++ Directionless/directionless.js
@@ -1,4 +1,4 @@
-/************** DRAW OBJECTS TO PLACE IN TILE MAP ******************/
+/**************** DRAW OBJECTS TO PLACE IN TILE MAP ********************/
 // Rocks (uses 'r' in tilemap)
 var rockObj = function(xPos, yPos) {
     this.x = xPos;
@@ -50,49 +50,78 @@ var dirtObj = function(posX, posY) {
 dirtObj.prototype.draw = function() {
     noStroke();
     // Draws dirt base
-    fill(191, 153, 40);
+    fill(189, 150, 66);
     rect(this.x, this.y, 20, 20);
     // Draws dirt details
-    fill(166, 129, 70);
-    ellipse(this.x+13, this.y+12, 8, 3);
-    ellipse(this.x+16, this.y+4, 3, 4);
-    rect(this.x, this.y+17, 20, 4);
-    rect(this.x, this.y, 3, 20);
-    rect(this.x+5, this.y+3, 6, 3);
     stroke(212, 198, 155);
-    line(this.x+16, this.y+5, this.x+13, this.y+15);
-    line(this.x+13, this.y+2, this.x+9, this.y+16);
-    line(this.x+8, this.y+3, this.x+6, this.y+10);
+    line(this.x+16, this.y+6, this.x+13, this.y+8);
+    line(this.x+13, this.y+5, this.x+9, this.y+10);
+    line(this.x+3, this.y+5, this.x+5, this.y+9);
+    line(this.x+6, this.y+14, this.x+8, this.y+16);
 };
 var dirtArr = [];
-/*******************************************************************/
+
+////////////////////////////////////////////////////
+
+// Draws the food (bread) on top of the mouse depending on mouse clicks
+// The food overlays the tilemap and its objects
+var foodObj = function(posX, posY) {
+    this.x = posX;
+    this.y = posY;
+};
+foodObj.prototype.draw = function() {
+    noStroke();
+    // Draws bread base
+    fill(204, 152, 63);
+    ellipse(this.x+10, this.y+10, 16, 10);
+    stroke(173, 124, 59);
+    arc(this.x+10, this.y+10, 16, 10, 0, 180);
+    ellipse(this.x+5, this.y+8, 1, 3);
+    ellipse(this.x+10, this.y+8, 1, 4);
+    ellipse(this.x+15, this.y+8, 1, 2);
+    noStroke();
+    fill(240, 208, 149);
+    ellipse(this.x+12, this.y+8, 2, 4);
+};
+var foodArr = [];
+/////////////////////////////////////TODO TODO TODO TODO//////////////////////////
+// Places food on the map depending on mouse click
+// mouseClicked = function() {
+//     foodArr.push(mouseX);
+// };
+/***********************************************************************/
 /*
     STRETCH GOAL: Add a setup menu for the tile map that allows
                   users to place the objects on the grid with
                   mouse clicks and object selection. Do not allow
                   more than 20 rocks/obstacles.
+                  ---
+    OPTIONAL:     Multi-layered tile maps; a layer dedicated to
+                  particular objects (rocks, grass, dirt, etc.)
+                  and differently scaled (20x20 for one layer,
+                  25x25 for next layer, 40x40 for the next, etc.)
 */
 // Used to initialize object's positions; separate from drawing them
-var tilemap = ["gddgggdd------rgggrr",
-               "ddgggddd-----ggggggg",
-               "dggdrrddddd---ggg---",
-               "ggggrddddd----------",
-               "ggggdddd------------",
-               "ggggggddd------r---d",
-               "ddggddd----------ddd",
-               "ddddd---------ggdddd",
-               "drgddggg----ggggggdd",
-               "drrgddggggggdgggggdd",
-               "drggggggggddddgggggd",
-               "g-ddddgggdddddddgggd",
-               "g--ddggrrrdddddddddd",
-               "g-gggddggddggddddddd",
-               "ggggddddgdddggggddgg",
-               "gggrrggdgdddgdddddgg",
-               "gggggggggdddddrddggg",
-               "ggggdddrrdddgddggggg",
-               "ddddggdrddggdddddggg",
-               "dddgdgdddddddggggggg"];
+var tilemap = ["gddggg----dd--rgg-rr",
+               "ddggg--d-----gggggg-",
+               "dggdrrdd-dd---gggddd",
+               "-gggrdd--d-----ddddd",
+               "gggg--dd----dd------",
+               "ggggggddd---dddr---d",
+               "d-ggddd------ddg-ddd",
+               "--dddg--------ggdddd",
+               "-rgddggd---gggggg-dd",
+               "-rrgdgg---ggdgg---dd",
+               "-rgggggg----ddg--ggd",
+               "-dddddggg---dd-dgggd",
+               "---ddggrrrdddddd--dd",
+               "g-gggddggddggd-----d",
+               "ggggddddgd--ggg-d---",
+               "-ggrrggdgg-d-dddddg-",
+               "--gggggggdddddrdd-gg",
+               "ggggdddrr-ddgdd---dg",
+               "d---ggdr--ggdddddggg",
+               "dddgdgd---dddgg---gg"];
 var initTilemap = function() {
     for(var i = 0; i < tilemap.length; i++) {
         for(var j = 0; j < tilemap[i].length; j++) {
@@ -121,16 +150,9 @@ var initTilemap = function() {
     }
 };
 
-//TODO
-/*
-character.move();
-character.checkCollsion();
-character.draw();
-*/
-
 initTilemap();
 draw = function() {
-    background(255, 255, 255);
+    background(189, 145, 79);
     
     for(var i = 0; i < rockArr.length; i++) {
         rockArr[i].draw();
@@ -142,15 +164,19 @@ draw = function() {
         dirtArr[i].draw();
     }
     
+    for(var i = 0; i < foodArr.length; i++) {
+        foodArr[i].draw();
+    }
+    
     //////////////////////////////////////////////////////////////////////////////////
     // TEMPORARY; PLACEHOLDER LINES TO LAY OUT GRID
     // 400 / 20 = 20
-    stroke(166, 166, 166);
-    for(var i = 20; i < 400; i = i + 20) {
-        line(0, i, width, i);
-    }
-    for(var i = 20; i < 400; i = i + 20) {
-        line(i, 0, i, height);
-    }
+    // stroke(166, 166, 166);
+    // for(var i = 20; i < 400; i = i + 20) {
+    //     line(0, i, width, i);
+    // }
+    // for(var i = 20; i < 400; i = i + 20) {
+    //     line(i, 0, i, height);
+    // }
     //////////////////////////////////////////////////////////////////////////////////
 };

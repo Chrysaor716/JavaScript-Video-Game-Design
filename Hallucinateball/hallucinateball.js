373f2d9f9819671d7c338b57b4584de7737b05f4
Christina Nguyen <phuonghien716@gmail.com>
2016-10-06 16:52:35 -0400

    Added simple bounce physics

--- Hallucinateball/hallucinateball.js
+++ Hallucinateball/hallucinateball.js
@@ -1,5 +1,55 @@
 angleMode = "radians";
 
+/************************* Game objects ****************************/
+var gravity = new PVector(0, 0.1); // apply graviational force on ball
+var ballObj = function(x, y) {
+    this.position = new PVector(x,y);
+    this.velocity = new PVector(0, 0);
+    this.acceleration = new PVector(0, 0);
+    this.size = 40;
+    this.mass = this.size / 5;
+};
+ballObj.prototype.applyForce = function(force) {
+    var f = PVector.div(force, this.mass);
+    this.acceleration.add(f);
+};
+ballObj.prototype.updatePosition = function() {
+    var gravityForce = PVector.mult(gravity, this.mass);
+    this.applyForce(gravityForce);
+    var airFriction = PVector.mult(this.velocity, -1);
+    airFriction.normalize();
+    airFriction.mult(0.02);
+    this.applyForce(airFriction);
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    // Bottom of canvas is the ground; bounce off the ground
+    if(this.position.y > (height - (this.size/2))) {
+        this.position.y = height - this.size/2;
+        this.velocity.y *= -1;
+    }
+    this.acceleration.set(0, 0);
+};
+/* STRETCH GOAL: MAKE THE BALL PRETTIER */
+ballObj.prototype.draw = function() {
+    noStroke();
+    fill(74, 74, 74);
+    ellipse(this.position.x, this.position.y, this.size, this.size);
+    fill(0, 61, 184);
+    arc(this.position.x-5, this.position.y, this.size+10, this.size, -Math.PI/3, Math.PI/3);
+    fill(105, 17, 130);
+    arc(this.position.x+5, this.position.y, this.size+10, this.size, 2*Math.PI/3, 4*Math.PI/3);
+    fill(74, 74, 74);
+    arc(this.position.x-5, this.position.y, this.size-10, this.size-10, -Math.PI/3, Math.PI/3);
+    arc(this.position.x+5, this.position.y, this.size-10, this.size-10, 2*Math.PI/3, 4*Math.PI/3);
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    fill(252, 252, 252, 1);
+    arc(this.position.x, this.position.y, this.size, this.size/2, Math.PI/6, 5*Math.PI/6);
+    arc(this.position.x, this.position.y, this.size, this.size/2, 7*Math.PI/6, 11*Math.PI/6);
+};
+var basketball = new ballObj(20, random(20, 380));
+/*******************************************************************/
+
 //////////////////////* Game states *//////////////////////
 var menuState = function() {}; // constructor
 menuState.prototype.execute = function() {
@@ -12,12 +62,11 @@ menuState.prototype.execute = function() {
 var playState = function() {}; // constructor
 playState.prototype.execute = function() {
     background(255, 255, 255);
-    fill(0, 0, 0);
-    textSize(30);
-    text("game state", width/2-70, height/2);
+    basketball.draw();
+    basketball.updatePosition();
 };
 //--------------------------------------------------------
-var gameObj = function() { 
+var gameObj = function() {
     this.state = [new menuState(), new playState()];
     this.currState = 0; // index 0 (menuState)
 };
@@ -27,7 +76,20 @@ gameObj.prototype.changeStateTo = function(state) {
 var game = new gameObj();
 //////////////////////////////////////////////////////////
 
+/***************** Game interaction (mouse clicks) ********************/
+mouseClicked = function() {
+    if(game.currState === 0) { // If user clicks on screen
+        game.changeStateTo(1); //   change to play state
+    } else if(game.currState === 1) {
+        // TODO
+    } else {
+        // Default
+    }
+};
+
+/**********************************************************************/
 
 draw = function() {
-    game.state[game.currState].execute();
+    // game.state[game.currState].execute();
+    game.state[1].execute();
 };

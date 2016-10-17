28c6218a3d47cc293fe0392c101d4527c3d5b93d
Christina Nguyen <phuonghien716@gmail.com>
2016-10-17 13:15:14 -0400

    Added strength variable for ball throw

--- Dodgeball/dodgeball.js
+++ Dodgeball/dodgeball.js
@@ -1,6 +1,10 @@
 angleMode = "radians";
 
+var strength = 0; // strength of the ball throw (depending on how long player holds mouse)
+var strengthLvl = 0; // user feedback for strength level (fraction of "strength")
+
 /*********************************** GAME OBJECTS ************************************/
+var gravity = new PVector(0, 0.1);
 var ballObj = function(x, y) {
     this.position = new PVector(x, y);
     this.velocity = new PVector(0, 0);
@@ -8,12 +12,31 @@ var ballObj = function(x, y) {
     this.size = 80;
     this.mass = this.size / 5;
 };
+ballObj.prototype.applyForce = function(force) {
+    var f = PVector.div(force, this.mass);
+    this.acceleration.add(f);
+};
+ballObj.prototype.updatePosition = function() {
+    var gravityForce = PVector.mult(gravity, this.mass);
+    this.applyForce(gravityForce);
+
+    var airFriction = PVector.mult(this.velocity, -1);
+    airFriction.normalize();
+    airFriction.mult(0.08);
+
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+
+    this.acceleration.set(0, 0); // reset acceleration each frame
+};
 ballObj.prototype.draw = function() {
     noStroke();
-    fill(255, 0, 0);
+    fill(16, 47, 168);
     ellipse(this.position.x, this.position.y, this.size, this.size);
+    fill(255, 255, 255);
+    ellipse(this.position.x-(this.size/2/2), this.position.y, this.size/2/2, this.size/2/2);
 };
-var ball = new ballObj(width/2, height/2);
+var ball = new ballObj(width/2, 350);
 /*************************************************************************************/
 
 /*********************************** GAME STATES ************************************/
@@ -48,7 +71,30 @@ gameObj.prototype.changeStateTo = function(state) {
 var game = new gameObj();
 /*************************************************************************************/
 
+mouseReleased = function() {
+    if(strength > 0 && strength <= 30) {
+        strengthLvl = 1;
+    } else if(strength > 30 && strength < 60) {
+        strengthLvl = 2;
+    } else if(strength >= 60) {
+        strengthLvl = 3;
+    }
+    strength = -1; // reset strength variable when mouse is released & ball is thrown
+};
+
+/*************************************************************************************/
+
 draw = function() {
     // game.state[game.currState].execute(game);
 game.state[1].execute(game);
+
+    if(mouseIsPressed) {
+        strength++;
+    }
+    // strength = 0; // reset strength variable
+    textSize(15);
+    fill(53, 51, 196);
+
+    text("Strength: " + strength, 300, 370);
+    text("Strength lvl: " + strengthLvl, 282, 390);
 };

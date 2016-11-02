e7823c457a5b78503b461fabc5ebb04d681a9784
Christina Nguyen <phuonghien716@gmail.com>
2016-11-02 17:45:56 -0400

    Added perlin background to menu screens & some text

--- identity.js
+++ identity.js
@@ -6,35 +6,35 @@ frameRate(60);
 /*
  *  Characters
  */
-var child = function(x, y, charType) {
+var childObj = function(x, y, charType) {
     this.position = new PVector(x, y);
     // Indicates whether the character is the original, its shadow, or its reflection
     this.charType = charType;
     this.size = 40; // default
     this.facing = 1; // default: character is facing right
-    
+
     // animation variables
     this.snapshot = 0;
     this.currFrame = frameCount;
     this.dir = 1; // direction of frame iterations for animation
 };
-child.prototype.draw = function() {
+childObj.prototype.draw = function() {
     pushMatrix();
     translate(this.position.x, this.position.y);
-    
+
     ///////////////////////////////////////////
-    // Collision box for child
-    stroke(0, 0, 0);
-    strokeWeight(1);
-    line(-this.size/2, -this.size/2, this.size/2, -this.size/2);
-    line(-this.size/2, this.size/2, this.size/2, this.size/2);
-    line(-this.size/2, -this.size/2, -this.size/2, this.size/2);
-    line(this.size/2, -this.size/2, this.size/2, this.size/2);
-    fill(0, 136, 255, 100);
-    noStroke();
-    ellipse(0, 0, this.size, this.size);
+    // // Collision box for child
+    // stroke(0, 0, 0);
+    // strokeWeight(1);
+    // line(-this.size/2, -this.size/2, this.size/2, -this.size/2);
+    // line(-this.size/2, this.size/2, this.size/2, this.size/2);
+    // line(-this.size/2, -this.size/2, -this.size/2, this.size/2);
+    // line(this.size/2, -this.size/2, this.size/2, this.size/2);
+    // fill(0, 136, 255, 100);
+    // noStroke();
+    // ellipse(0, 0, this.size, this.size);
     ///////////////////////////////////////////
-    
+
     noStroke();
     fill(222, 187, 104);
     ellipse(0, -this.size/4, this.size/2, this.size/2); //head
@@ -49,7 +49,7 @@ child.prototype.draw = function() {
     ellipse(this.size/6 * this.facing, -this.size/4, this.size/12, this.size/6); //eye
     fill(222, 187, 104);
     ellipse(0, -this.size/4, this.size/6, this.size/5); //ear
-    
+
     ////////////////////////  TODO jump animations ////////////////////////////////
     switch(this.snapshot) {
         case 0:
@@ -57,113 +57,113 @@ child.prototype.draw = function() {
             strokeWeight(this.size/8);
             line(0, 0, -this.size/4 * this.facing, this.size/4); //arms (back)
             line(0, this.size/3, this.size/4 * this.facing, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, this.size/4 * this.facing, this.size/4); //arms (front)
             line(0, this.size/3, -this.size/4 * this.facing, this.size/2); //leg (front)
         break;
-        
+
         case 1:
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, -this.size/6 * this.facing, this.size/4); //arms (back)
             line(0, this.size/3, this.size/6 * this.facing, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, this.size/6 * this.facing, this.size/4); //arms (front)
             line(0, this.size/3, -this.size/6 * this.facing, this.size/2); //leg (front)
         break;
-        
+
         case 2:
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, -this.size/8 * this.facing, this.size/4); //arms (back)
             line(0, this.size/3, this.size/8 * this.facing, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, this.size/8 * this.facing, this.size/4); //arms (front)
             line(0, this.size/3, -this.size/8 * this.facing, this.size/2); //leg (front)
         break;
-        
+
         case 3:
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, 0, this.size/4); //arms (back)
             line(0, this.size/3, 0, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, 0, this.size/4); //arms (front)
             line(0, this.size/3, 0, this.size/2); //leg (front)
         break;
-        
+
         case 4:
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, -this.size/-8 * this.facing, this.size/4); //arms (back)
             line(0, this.size/3, this.size/-8 * this.facing, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, this.size/-8 * this.facing, this.size/4); //arms (front)
             line(0, this.size/3, -this.size/-8 * this.facing, this.size/2); //leg (front)
         break;
-        
+
         case 5:
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, -this.size/-6 * this.facing, this.size/4); //arms (back)
             line(0, this.size/3, this.size/-6 * this.facing, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, this.size/-6 * this.facing, this.size/4); //arms (front)
             line(0, this.size/3, -this.size/-6 * this.facing, this.size/2); //leg (front)
         break;
-        
+
         case 6:
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, -this.size/-4 * this.facing, this.size/4); //arms (back)
             line(0, this.size/3, this.size/-4 * this.facing, this.size/2); //leg (back)
-    
+
             noStroke();
             fill(54, 64, 255);
             ellipse(0, this.size/5, this.size/2, this.size/2); //torso
-    
+
             stroke(222, 187, 104);
             strokeWeight(this.size/8);
             line(0, 0, this.size/-4 * this.facing, this.size/4); //arms (front)
             line(0, this.size/3, -this.size/-4 * this.facing, this.size/2); //leg (front)
         break;
-        
+
         default:
             //
         break;
@@ -182,13 +182,27 @@ child.prototype.draw = function() {
         this.dir = -this.dir;
     }
     // println(this.snapshot);
-    
+
     popMatrix();
 };
-var boy = new child(width/2, height/2);
+var boy = new childObj(width/2, height/2 ,1);
 // TODO: make shadow
 // TODO: make reflection
 ////////////////////////////////////
+
+/*
+ *  Draws mountains in the background using Perlin noise
+ */
+var mountainObj = function() {};
+mountainObj.prototype.draw = function() {
+    stroke(222, 222, 222);
+    var step = 0.01;
+    for(var t = 0; t < step*width; t += step) {
+        var y = map(noise(t), 2, 1, 0, height/3);
+        rect(t*100, height-y, 1, y);
+    }
+};
+var mountains = new mountainObj();
 
 /*
  *	Tilemaps
@@ -257,18 +271,41 @@ initRockTilemap();
 var menuPage1 = function() {}; // constructor
 menuPage1.prototype.execute = function(obj) {
 	background(255, 255, 255);
-	fill(0, 0, 0);
-	textSize(20);
-	text("Menu page 1.", width/2-70, height/2-100);
+    mountains.draw();
 
+	fill(30, 0, 222);
+	textSize(40);
+    textFont(createFont("monospace"));
+	text("Identity", width/2-100, 60);
+
+	fill(0, 0, 0);
+	textSize(15);
+	text("Child, where is your imagination going today? Who will\n" +
+	"  you be? What will you do? Where will you venture?", 60, 100);
+
+    boy.position.set(width/2, height/2+50);
 	boy.draw();
+
+	fill(0, 0, 0);
+	text("Click to advance...", 230, 350);
 };
 var menuPage2 = function() {};
 menuPage2.prototype.execute = function(obj) {
 	background(255, 255, 255);
+    mountains.draw();
+
 	fill(0, 0, 0);
+	textSize(15);
+	textFont(createFont("monospace"));
+	text("You are what you make yourself out to be, child. Use the\n" +
+	"arrow keys to advance. Your alternate ego is with you...", 60, 60);
+
+	fill(0, 30, 255);
 	textSize(20);
-	text("Menu page 2.", width/2-70, height/2);
+	text("Identity", width/2-50, 300);
+	fill(0, 0, 0);
+	textSize(12);
+	text("Christina Nguyen", width/2-60, 320);
 };
 
 /////////////////////////  TODO  /////////////////////////
@@ -277,7 +314,7 @@ gameState.prototype.execute = function(obj) {
 	background(255, 255, 255);
 	fill(0, 0, 0);
 	textSize(20);
-	text("Game state.", width/2-70, height/2);
+	text("Game state (in progress)", width/2-100, height/2);
 
 	for(var i = 0; i < rockArr.length; i++) {
 		rockArr[i].draw();
@@ -307,6 +344,5 @@ mouseClicked = function() {
 draw = function() {
 	game.state[game.currState].execute(game);
 };
-
 
 }};
